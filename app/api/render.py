7a0f9acfddad3f7f4851e7db874df114d35f9080
pythexcel <pythexcel@excellencetechnologies.in>
2020-02-24 20:16:52 +0530

    home contact about

--- app/api/render.py
+++ app/api/render.py
@@ -11,8 +11,22 @@ def index(username):
     return render_template('index.html', username=username)
 
 
-@bp.route("/page1")
-def template_test():
-    return render_template('index1.html', my_string="Wheeeee!", my_list=[0,1,2,3,4,5])
 
 
+@bp.route("/home")
+def home():
+    return render_template(
+        'index1.html', my_string="Wheeeee!", 
+        my_list=[0,1,2,3,4,5], title="Home")
+
+@bp.route("/about")
+def about():
+    return render_template(
+        'about.html', my_string="Wheeeee!", 
+        my_list=[0,1,2,3,4,5], title="About")
+
+@bp.route("/contact")
+def contact():
+    return render_template(
+        'contact.html', my_string="Wheeeee!", 
+        my_list=[0,1,2,3,4,5], title="Contact Us")
